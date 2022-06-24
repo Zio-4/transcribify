d8d2132889c319453f8e394c3b9616944789a22f
Philip Ziolkowski <philipz4848@gmail.com>
2022-06-24 02:01:32 -0700

    Adding button element, onChange event handler function, and loading component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,52 @@ import logo from './logo.svg';
 import './App.css';
 import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
+import { useState } from 'react';
+import Loading from './Loading';
 
 const Input = styled('input')({
   display: 'none',
 });
 
-const onChange = (e) => {
-  console.log('file: ', e.target.files)
-}
+
 
 
 function App() {
+  const [file, setFile] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [transcription, setTranscription] = useState('')
+  const axios = require('axios');
+
+  const assembly = axios.create({
+    baseURL: "https://api.assemblyai.com/v2",
+    headers: {
+        authorization: "935d974879134d49b21a0bca7fd81bd8",
+        "content-type": "application/json",
+        "transfer-encoding": "chunked",
+    },
+  });
+
+  const onChange = (e) => {
+    if (!e.target.files[0]) return
+    setFile(e.target.files[0].name)
+    setLoading(true)
+
+    console.log('file: ', e.target.files[0])
+  }
+
+
+
   return (
     <div className="App">
+      <div>{file}</div>
       <label htmlFor="contained-button-file">
-        <Input accept="image/*" id="contained-button-file" multiple type="file" onChange={onChange}/>
+        <Input accept="audio/*,video/*" id="contained-button-file" multiple type="file" onChange={onChange}/>
         <Button variant="contained" component="span">
           Upload
         </Button>
       </label>
+      {loading ? <div>Your file is being transcribed...</div> : <div>Upload a file to transcribe!</div>}
+      {loading ? <Loading/> : null}
     </div>
   );
 }
---
+++ src/Loading.js
@@ -0,0 +1,22 @@
+import Skeleton from '@mui/material/Skeleton';
+
+import React from 'react'
+
+function Loading() {
+  return (
+    <div>
+    <Skeleton width={300}/>
+    <Skeleton width={300}/>
+    <Skeleton width={300}/>
+    <Skeleton width={300}/>
+    <Skeleton width={300}/>
+    <Skeleton width={300}/>
+    <Skeleton width={300}/>
+    <Skeleton width={300}/>
+    <Skeleton width={300}/>
+    <Skeleton width={300}/>
+    </div>
+  )
+}
+
+export default Loading
